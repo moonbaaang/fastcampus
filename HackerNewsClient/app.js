8c1798075334b63f73beeee8d3696b436c9d59bf
moonbaaang <qudwjsdl0814@naver.com>
2021-07-28 00:42:28 +0900

    Update paging process

--- HackerNewsClient/app.js
+++ HackerNewsClient/app.js
@@ -1,11 +1,14 @@
-//let ajax = new XMLHttpRequest();
-// https://developer.mozilla.org/ko/docs/Web/API/XMLHttpRequest 참고
+// 라우터 (화면 처리기) 추가
 
 const container = document.getElementById('root');
 const ajax = new XMLHttpRequest(); // 다른 데이터로 변경 불가 (상수)
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json'; // 바뀔 가능성이 있는 변수는 따로 빼주는게 좋음
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'; //값을 확정할 수 없음
+const store = {
+    currentPage: 1,
+    lastPage: 0,
+}
 
 
 function getData(url){
@@ -14,57 +17,68 @@ function getData(url){
 
     return JSON.parse(ajax.response)
 }
-//ajax.open('GET', url, false);
-//ajax.send(); >> 위의 함수화
 
-//console.log(ajax.response);
-const newsFeed = getData(NEWS_URL)
-//console.log(newsFeed)
-const ul = document.createElement('ul');
+function newsFeed(){
+    const newsFeed = getData(NEWS_URL)
+    const newsList = [];
+    store.lastPage = newsFeed.length%10 == 0? newsFeed.length/10:newsFeed.length%10+1
+    console.log("store.lastPage : "+store.lastPage)
 
-window.addEventListener('hashchange', function(){
-    //console.log("해시가 변경됨");
-    const id = location.hash.substr(1); // 1부터 반환
-    
-    
-    //ajax.open('GET', CONTENT_URL.replace('@id', id), false); //@id를 변경
-    //ajax.send();
+    newsList.push('<ul>')
 
-    //const newsContent = JSON.parse(ajax.response)
-    const newsContent = getData(CONTENT_URL.replace('@id', id))
-    const title = document.createElement('h1')
-
-    title.innerHTML = newsContent.title
-    content.appendChild(title)
-    console.log(newsContent)
-});
-
-for(let i=0 ; i < 10 ; i++){
-    const div = document.createElement('div')
-    const li = document.createElement('li');
-    const a = document.createElement('a')
-    //<li>${newsFeed[i].title}</li> 하드코딩 시
-
-    div.innerHTML = 
-    `
-    <li>
-        <a href='#${newsFeed[i].id}'>
-            ${newsFeed[i].title} (${newsFeed[i].comments_count})
-        </a>
-    </li>
-    `
-
-    //a.href = `#${newsFeed[i].id}`;
-    //a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
-    
-    //a.addEventListener('click', function(){});
-
-    //li.appendChild(a);
-    //ul.appendChild(div.children[0]); //임시로 div 태그의 DOM형태를 이용하고 div를 버림
-    ul.appendChild(div.firstElementChild); // 위와 같은 표현
+    for(let i = (store.currentPage-1)*10 ; i < store.currentPage*10 ; i++){
+        newsList.push( `
+        <li>
+            <a href='#/show/${newsFeed[i].id}'>
+                ${newsFeed[i].title} (${newsFeed[i].comments_count})
+            </a>
+        </li>
+        `);
+    }
+ 
+    newsList.push('</ul>')
+    newsList.push(`
+        <div>
+            <a href='#/page/${store.currentPage>1 ? store.currentPage-1 : 1}'>이전페이지</a>
+            <a href='#/page/${store.currentPage<store.lastPage ? store.currentPage+1 : store.lastPage}'>다음페이지</a>
+        </div>
+    `);
+    container.innerHTML = newsList.join("") // 배열 요소를 모두 합쳐 반환 ,(콤마로 기본 연결)
 }
 
 
+function newsDetail(){
+    const id = location.hash.substr(7); // 1부터 반환
+    
+    const newsContent = getData(CONTENT_URL.replace('@id', id))
 
-container.appendChild(ul)
-container.appendChild(content)
+    container.innerHTML = `
+    <h1>
+        ${newsContent.title}
+    </h1>
+    <div>
+        <a href="#/page/${store.currentPage}">목록으로</a>
+    </div>
+    `;
+}
+
+function router(){
+    const routePath = location.hash;
+
+    if (routePath == ''){
+        newsFeed()
+    } else if(routePath.indexOf('#/page/')>=0) {
+        store.currentPage = Number(routePath.substr(7));
+        newsFeed()
+    } else {
+        newsDetail()
+    }
+}
+
+window.addEventListener('hashchange', router);
+
+router();
+
+
+
+
